a6c7499cf78e4c474bf06af0557d228a1ff4d5a6
Максим К <@HOME-PC>
2024-09-17 17:01:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Lessons_;
+internal class Program
+{
+    private static void Main()
+    {
+        /*int[] ints = new int[10];
+        foreach (var item in ints)
+        {
+            Console.Write(item + " ");
+        }*/
+
+        // Nullable<int> b = 1;
+        int? nullableInt = null;
+
+
+
+        int?[] nulls = new int?[10];
+        foreach (var item in nulls)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
